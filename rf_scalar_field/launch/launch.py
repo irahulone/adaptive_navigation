3faedb97ddd9958419ee2ae952083c01e0a22840
Christian Pedrigal <cpedrigal@scu.edu>
2025-05-02 06:43:19 +0000

    adds rf_receiver to launch file

--- rf_scalar_field/launch/launch.py
+++ rf_scalar_field/launch/launch.py
@@ -5,20 +5,12 @@ from launch_ros.actions import Node
 import os
 from ament_index_python.packages import get_package_share_directory
 
-# TODO:
-"""
-Edit the following:
-
-- my_package
-- my_node
-
-- robot_speed
-- use_sensors
-- controller_gain
-
-"""
-
 def generate_launch_description():
+    port1 = LaunchConfiguration('port1')
+    freq1 = LaunchConfiguration('freq1')
+    port2 = LaunchConfiguration('port2')
+    freq2 = LaunchConfiguration('freq2')
+
     config_file = os.path.join(
         get_package_share_directory('rf_scalar_field'),
         'config',
@@ -26,8 +18,10 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
-        DeclareLaunchArgument('port', default_value='/dev/ttyUSB2'),
-        DeclareLaunchArgument('freq', default_value='1.0'),
+        DeclareLaunchArgument('port1', default_value='/dev/ttyUSB2'),
+        DeclareLaunchArgument('freq1', default_value='1.0'),
+        DeclareLaunchArgument('port2', default_value='/dev/ttyUSB1'),
+        DeclareLaunchArgument('freq2', default_value='1.0'),
 
         Node(
             package='rf_scalar_field',
@@ -35,10 +29,17 @@ def generate_launch_description():
             name='rf_source',
             parameters=[
                 config_file,
-                {
-                    'port': LaunchConfiguration('port'),
-                    'freq': LaunchConfiguration('freq'),
-                }
+                {'port': port1, 'freq': freq1}
             ]
-        )
+        ),
+        Node(
+            package='rf_scalar_field',
+            executable='rf_receiver',
+            name='rf_receiver',
+            parameters=[
+                config_file,
+                {'port': port2, 'freq': freq2}
+            ]
+        ),
     ])
+
