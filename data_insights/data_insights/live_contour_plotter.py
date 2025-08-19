5e1d5625b0b3d26dbc0c12115ecf80cc87116130
Christian Pedrigal <cpedrigal@scu.edu>
2025-08-19 00:28:23 +0000

    migrate statistics code from live_contour_plotter.py to collect_gps_rssi

--- data_insights/data_insights/live_contour_plotter.py
+++ data_insights/data_insights/live_contour_plotter.py
@@ -2,153 +2,47 @@ import rclpy
 from rclpy.node import Node
 from adaptive_navigation_interfaces.msg import Contour
 
-from typing import List, Any, Callable
 
-from builtin_interfaces.msg import Time
 from std_msgs.msg import Int16
 from geometry_msgs.msg import Pose2D
 
 import matplotlib.pyplot as plt
 import numpy as np
-
-from math import inf
+from typing import Callable, Any, Union
+import os
 
 from adaptive_navigation_utilities.pubsub import PubSubManager
 
 
-class Data:
-    def __init__(self):
-        self.data: List = []
-    
-    @property
-    def size(self):
-        return len(self.data)
-    
-    def _insert(self, data: Any, ind: int) -> bool:
-        try:
-            if ind >= self.size:
-                self.data.append(data)
-            else:
-                self.data[ind] = data
-            return True
-        except Exception as e:
-            print(e)
-            return False
-    
-    def append(self, data: Any) -> bool:
-        try:
-            self.data.append(data)
-            return True
-        except Exception as e:
-            print(e)
-            return False
-
-class ContinuousData(Data):
-    def __init__(self):
-        super().__init__()
-        self._precision: float = inf
-        self._p1: int = -1
-        self._p2: int = -2
-        self._precision_ind_1 = 0
-        self._precision_ind_2 = 0
-
-    @property
-    def max(self):
-        return np.max(self.data)
-
-    @property
-    def min(self):
-        return np.min(self.data)
-
-    @property
-    def avg(self):
-        return np.average(self.data)
-    
-    @property
-    def std(self):
-        return np.std(self.data)
-    
-    @property
-    def precision(self):
-        return self._precision
-
-    def _update_precision(self) -> bool:
-
-        # Need at least two measurements
-        if len(self.data) >= 2:
-
-            # Get current difference between last 2 successive measurments
-            diff: float = abs(self.data[self._p1] - self.data[self._p2])
-
-            # Update precision if difference is less than current
-            if diff < self.precision:
-                self._precision = diff 
-                self._precision_ind_1 = self._p1
-                self._precision_ind_2 = self._p2
-            
-            return True
-        else:
-            return False
-
-    def append(self, data) -> bool:
-
-        # Perform append as usual
-        if super().append(data):
-
-            # Update precision
-            if self._update_precision():
-                return True
-            else:
-                return False
-            
-    def _insert(self, data: Any, ind: int) -> bool:
-
-        # Perform insert as usual
-        if super()._insert(data, ind):
-
-            # Update precision
-            if self._update_precision():
-                return True
-            else:
-                return False
-
-
-class MovingAverage(ContinuousData):
-    def __init__(self, buffer: int = 10):
-        super().__init__()
-        self._buffer: int = buffer
-        self._ind: int = 0
-
-    @property
-    def buffer(self):
-        return self._buffer
-    
-    @buffer.setter
-    def buffer(self, buffer):
-        self._buffer: float = buffer
-
-    def append(self, data: Any) -> bool:
-
-        # Update precision indices
-        self._p1 = self._ind
-        self._p2 = self._ind - 1 % self.buffer
-
-        # Insert data
-        super()._insert(data, self._ind)
-        
-        # Wrap index if larger than bugger
-        self._ind = (self._ind + 1) % self.buffer
-
-
 class ContourPlotter(Node):
 
-    RSSI_SUB_TOPIC: str = "rssi"
+    CONTOUR_SUB_TOPIC: str = "contour_sub_topic"
+    ROBOT_ID: str = "robot_id"
 
     def __init__(self):
-        super().__init__('contour_plotter')
+
+        # Get robot ID from parameter or environment
+        robot_id = os.getenv("ROBOT_ID", "")
+
+        super().__init__("_".join([robot_id, __name__.split('.')[-1]]))
 
         # Create PubSubManager
         self.pubsub: PubSubManager = PubSubManager(self)
+
+        # Declare parameters provided with default params
+        # if not using ros2 launch
+        self.declare_parameters(
+            namespace='', # TODO: Include parameters here??
+            parameters=[
+                (ContourPlotter.CONTOUR_SUB_TOPIC, "contour"),
+                (ContourPlotter.ROBOT_ID, robot_id)
+            ]
+        )
+
+        # Set attributes from parameters
+        # Note: Parameters are at the node level
+        #       while attributes are the class level  
+        self.robot_id = self.get(ContourPlotter.ROBOT_ID)
 
         # Create publish topics
         self.create_subscriptions()
@@ -165,23 +59,6 @@ class ContourPlotter(Node):
 
         self.latest_data = None
 
-        # Meters per change in RSSI
-        self.last_x: float = 0.0
-        self.last_y: float = 0.0
-        self.last_z: float = 0.0
-
-        # Update frequency
-        self.freq_buffer: int = 20
-        self.freq: MovingAverage = MovingAverage(self.freq_buffer)
-        # self.freq: ContinuousData = ContinuousData()
-        self.prev_stamp: float = 0.0
-
-        # Distance per RSSI
-        self.dist_per_rssi_buffer: int = 20
-        self.dist_per_rssi: MovingAverage = MovingAverage(self.dist_per_rssi_buffer)
-        # self.dist_per_rssi: ContinuousData = ContinuousData()
-
-
         # Set up matplotlib
         plt.ion()
         self.fig = plt.figure()
@@ -196,15 +73,8 @@ class ContourPlotter(Node):
     def create_subscriptions(self) -> None:
         self.pubsub.create_subscription(
             Contour,
-            'contour',
+            self.prepend(self.get(ContourPlotter.CONTOUR_SUB_TOPIC)),
             self.listener_callback,
-            10
-        )
-        
-        self.pubsub.create_subscription(
-            Int16,
-            ContourPlotter.RSSI_SUB_TOPIC, 
-            self.update_rssi_stats,
             10
         )
 
@@ -212,27 +82,9 @@ class ContourPlotter(Node):
     def log(self):
         return self.get_logger()
 
-    def update_rssi_stats(self, msg):
-
-        current_stamp: float = self.extract_time(self.get_clock().now().to_msg())
-        self.freq.append(round(1/(current_stamp - self.prev_stamp), 3))
-        self.prev_stamp = current_stamp
-
-        self.info(f"Avg: {self.freq.avg}")
-        self.info(f"Std: {self.freq.std}")
-        self.info(f"Min: {self.freq.min}")
-        self.info(f"Max: {self.freq.max}")
-        self.info(f"Size: {self.freq.size}")  
-        self.info(f"Ind: {self.freq._ind}")
-        self.info(f"Precision: {self.freq.precision}")
-        self.info(f"Precision Index 1: {self.freq._precision_ind_1}")
-        self.info(f"Precision Index 2: {self.freq._precision_ind_2}")
-        self.info(f"Data: {self.freq.data}")
-        self.info("~~~~~~~~~~~~~~~~~~~~~~~")
-
-    @staticmethod
-    def get_norm2_distance(vec1, vec2) -> np.float64:
-        return np.linalg.norm(vec1 - vec2)
+    # Create alias function for common ROS functions
+    def get(self, param) -> Any:
+        return self.get_parameter(param).value
 
     def listener_callback(self, msg: Contour):
         # if len(msg.x) != len(msg.y) or len(msg.x) != len(msg.z):
@@ -241,41 +93,9 @@ class ContourPlotter(Node):
 
         # self.info(f"X:{msg.x}, Y:{msg.y}, Z:{msg.z}")
 
-        # If there is a change in RSSI
-        if msg.z != self.last_z:
-
-            # Get Norm 2 distance
-            vec1: np.ndarray = np.array([msg.x, msg.y])
-            vec2: np.ndarray = np.array([self.last_x, self.last_y])
-            dist_per_unit_rssi: np.float64 = self.get_norm2_distance(vec1, vec2)
-
-            # Update Change per RSSI
-            self.dist_per_rssi.append(dist_per_unit_rssi)
-        
-
-            self.info(f"Dist Per RSSI Avg: {self.dist_per_rssi.avg}")
-            self.info(f"Dist Per RSSI Std: {self.dist_per_rssi.std}")
-            self.info(f"Dist Per RSSI Min: {self.dist_per_rssi.min}")
-            self.info(f"Dist Per RSSI Max: {self.dist_per_rssi.max}")
-            self.info(f"Dist Per RSSI Size: {self.dist_per_rssi.size}")  
-            self.info(f"Dist Per RSSI Ind: {self.dist_per_rssi._ind}")
-            self.info(f"Precision: {self.dist_per_rssi.precision}")
-            self.info(f"Precision Index 1: {self.dist_per_rssi._precision_ind_1}")
-            self.info(f"Precision Index 2: {self.dist_per_rssi._precision_ind_2}")
-            self.info(f"Dist Per RSSI Data: {self.dist_per_rssi.data}")
-            self.info("~~~~~~~~~~~~~~~~~~~~~~~")
-
-
-            # Update last RSSI value
-            self.last_y = msg.x
-            self.last_y = msg.y
-            self.last_z = msg.z
-
         self.latest_data = (np.array(msg.x), np.array(msg.y), np.array(msg.z))
 
-    def extract_time(self, time: Time, digits: int = 3) -> float:
-        return round(time.sec + time.nanosec * 1e-9, digits)
-
+  
     def update_plot(self):
         if self.latest_data is None:
             return
@@ -298,6 +118,11 @@ class ContourPlotter(Node):
 
         except Exception as e:
             self.error(f"Plot error: {e}")
+
+    # TODO: Replace with namespace builder
+    def prepend(self, topic: str) -> str:
+        return f"{self.robot_id}/{topic}"
+
 
 def main(args=None):
     rclpy.init(args=args)
--- synchronizer/synchronizer/collect_gps_rssi.py
+++ synchronizer/synchronizer/collect_gps_rssi.py
@@ -3,16 +3,151 @@ from rclpy.node import Node
 
 from typing import Callable, Any, Union
 
+from builtin_interfaces.msg import Time
 from sensor_msgs.msg import NavSatFix
-from std_msgs.msg import Int16
+from std_msgs.msg import Int16, Float64
 from geometry_msgs.msg import Pose2D
 
+import os
+import numpy as np
+from typing import List, Any, Callable
+from functools import partial
 
-from math import nan
+from math import nan, inf
 
 from adaptive_navigation_interfaces.msg import Contour
 from adaptive_navigation_utilities.pubsub import PubSubManager
 from adaptive_navigation_utilities.namespace_builder import HardwareNamespace
+
+
+class StringWrapper(str):
+    def prepend(self, str):
+        return self.__class__(str + self)
+    
+
+class Data:
+    def __init__(self):
+        self.data: List = []
+    
+    @property
+    def size(self):
+        return len(self.data)
+    
+    def _insert(self, data: Any, ind: int) -> bool:
+        try:
+            if ind >= self.size:
+                self.data.append(data)
+            else:
+                self.data[ind] = data
+            return True
+        except Exception as e:
+            print(e)
+            return False
+    
+    def append(self, data: Any) -> bool:
+        try:
+            self.data.append(data)
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
+class ContinuousData(Data):
+    def __init__(self):
+        super().__init__()
+        self._precision: float = inf
+        self._p1: int = -1
+        self._p2: int = -2
+        self._precision_ind_1 = 0
+        self._precision_ind_2 = 0
+
+    @property
+    def max(self):
+        return np.max(self.data)
+
+    @property
+    def min(self):
+        return np.min(self.data)
+
+    @property
+    def avg(self):
+        return np.average(self.data)
+    
+    @property
+    def std(self):
+        return np.std(self.data)
+    
+    @property
+    def precision(self):
+        return self._precision
+
+    def _update_precision(self) -> bool:
+
+        # Need at least two measurements
+        if len(self.data) >= 2:
+
+            # Get current difference between last 2 successive measurments
+            diff: float = abs(self.data[self._p1] - self.data[self._p2])
+
+            # Update precision if difference is less than current
+            if diff < self.precision:
+                self._precision = diff 
+                self._precision_ind_1 = self._p1
+                self._precision_ind_2 = self._p2
+            
+            return True
+        else:
+            return False
+
+    def append(self, data) -> bool:
+
+        # Perform append as usual
+        if super().append(data):
+
+            # Update precision
+            if self._update_precision():
+                return True
+            else:
+                return False
+            
+    def _insert(self, data: Any, ind: int) -> bool:
+
+        # Perform insert as usual
+        if super()._insert(data, ind):
+
+            # Update precision
+            if self._update_precision():
+                return True
+            else:
+                return False
+
+
+class MovingAverage(ContinuousData):
+    def __init__(self, buffer: int = 10):
+        super().__init__()
+        self._buffer: int = buffer
+        self._ind: int = 0
+
+    @property
+    def buffer(self):
+        return self._buffer
+    
+    @buffer.setter
+    def buffer(self, buffer):
+        self._buffer: float = buffer
+
+    def append(self, data: Any) -> bool:
+
+        # Update precision indices
+        self._p1 = self._ind
+        self._p2 = self._ind - 1 % self.buffer
+
+        # Insert data
+        super()._insert(data, self._ind)
+        
+        # Wrap index if larger than bugger
+        self._ind = (self._ind + 1) % self.buffer
+
 
 # Global variables
 DEBUG: bool = False
@@ -30,7 +165,10 @@ class SyncPublisher(Node):
     GPS_SUB_TOPIC: str = "gps_sub_topic"
     RSSI_SUB_TOPIC: str = "rssi_sub_topic"
     CONTOUR_PUB_TOPIC: str = "contour_sub_topic"
+    DIST_PER_UNIT_RSSI_PUB_TOPIC: str = "dist_per_unit_rssi_pub_topic"
+    UPDATE_FREQ_PUB_TOPIC: str = "update_freq_pub_topic"
     POSE_SUB_TOPIC: str = "pose_sub_topic"
+    ROBOT_ID: str = "robot_id"
     FREQ: str = "freq"
 
     def __init__(self):
@@ -39,7 +177,11 @@ class SyncPublisher(Node):
         # Note: We use the __name__ macro and
         # parse for the file name to use as
         # the node name
-        super().__init__(__name__.split('.')[-1])
+
+        # Get robot ID from parameter or environment
+        robot_id = os.getenv("ROBOT_ID", "")
+        
+        super().__init__("_".join([robot_id, __name__.split('.')[-1]]))
         
         # Pubsub
         self.pubsub: PubSubManager = PubSubManager(self)
@@ -57,6 +199,7 @@ class SyncPublisher(Node):
         self.declare_parameters(
             namespace='', # TODO: Include parameters here??
             parameters=[
+                (SyncPublisher.ROBOT_ID, robot_id),
                 (SyncPublisher.X_NAME, 0.0),
                 (SyncPublisher.Y_NAME, 0.0),
                 (SyncPublisher.Z_NAME, 0.0),
@@ -67,15 +210,37 @@ class SyncPublisher(Node):
                 (SyncPublisher.RSSI_SUB_TOPIC, "rssi"),
                 (SyncPublisher.CONTOUR_PUB_TOPIC, "contour"),
                 (SyncPublisher.FREQ, 5.0),
-                (SyncPublisher.POSE_SUB_TOPIC, "None/pose2D")
+                (SyncPublisher.POSE_SUB_TOPIC, "pose2D"),
+                (SyncPublisher.DIST_PER_UNIT_RSSI_PUB_TOPIC, "dist_per_rssi"),
+                (SyncPublisher.UPDATE_FREQ_PUB_TOPIC, "rssi_freq")
             ]
         )
+
 
         # Set attributes from parameters
         # Note: Parameters are at the node level
         #       while attributes are the class level
-        self.freq = self.get(SyncPublisher.FREQ)
+        self.timer_freq = self.get(SyncPublisher.FREQ)
+        self.robot_id = self.get(SyncPublisher.ROBOT_ID)
+
+        # Update frequency
+        self.freq_buffer: int = 20
+        self.freq: MovingAverage = MovingAverage(self.freq_buffer)
+        # self.freq: ContinuousData = ContinuousData()
+        self.prev_stamp: float = 0.0
         
+        # Meters per change in RSSI
+        self.last_x: float = 0.0
+        self.last_y: float = 0.0
+        self.last_z: float = 0.0
+
+
+        # Distance per RSSI
+        self.dist_per_rssi_buffer: int = 20
+        self.dist_per_rssi: MovingAverage = MovingAverage(self.dist_per_rssi_buffer)
+        # self.dist_per_rssi: ContinuousData = ContinuousData()
+
+
         # Add aliases for logging
         self.info: Callable = self.log().info
         self.warn: Callable = self.log().warn
@@ -97,7 +262,7 @@ class SyncPublisher(Node):
 
     @property
     def period(self) -> Numeric:
-        return 1/self.freq
+        return 1/self.timer_freq
 
     # Subscribes to all topics
     def subscribe_to_topics(self) -> None:
@@ -131,9 +296,26 @@ class SyncPublisher(Node):
         # Create Contour topic
         self.pubsub.create_publisher(
             Contour,
-            self.get(SyncPublisher.CONTOUR_PUB_TOPIC),
+            self.prepend(self.get(SyncPublisher.CONTOUR_PUB_TOPIC)),
             10
         )
+
+        # Create Distance per unit RSSI pub topic
+        self.pubsub.create_publisher(
+            Float64,
+            self.prepend(self.get(SyncPublisher.DIST_PER_UNIT_RSSI_PUB_TOPIC)),
+            10
+        )
+
+        # Create update frequency topic
+        self.pubsub.create_publisher(
+            Float64,
+            self.prepend(self.get(SyncPublisher.UPDATE_FREQ_PUB_TOPIC))
+        )
+
+    # TODO: Replace with namespace builder
+    def prepend(self, topic: str) -> str:
+        return f"{self.robot_id}/{topic}"
 
     # Publish contour data
     def publish_contour(self) -> None:
@@ -152,12 +334,56 @@ class SyncPublisher(Node):
         # Include timestamp
         contour.header.stamp = self.get_clock().now().to_msg()
 
+        # Update dist per unit RSSI
+        self.update_dist_per_unit_rssi()
+
         # Publish contour
         self.pubsub.publish(
-            self.get(SyncPublisher.CONTOUR_PUB_TOPIC),
+            self.prepend(self.get(SyncPublisher.CONTOUR_PUB_TOPIC)),
             contour
         )
     
+    @staticmethod
+    def get_norm2_distance(vec1, vec2) -> np.float64:
+        return np.linalg.norm(vec1 - vec2)
+
+
+    def update_dist_per_unit_rssi(self):
+
+        # If there is a change in RSSI
+        if self.z != self.last_z:
+
+            # Get Norm 2 distance
+            vec1: np.ndarray = np.array([self.x, self.y])
+            vec2: np.ndarray = np.array([self.last_x, self.last_y])
+            dist_per_unit_rssi: np.float64 = self.get_norm2_distance(vec1, vec2)
+
+            # Update Change per RSSI
+            self.dist_per_rssi.append(dist_per_unit_rssi)
+        
+
+            self.info(f"Dist Per RSSI Avg: {self.dist_per_rssi.avg}")
+            self.info(f"Dist Per RSSI Std: {self.dist_per_rssi.std}")
+            self.info(f"Dist Per RSSI Min: {self.dist_per_rssi.min}")
+            self.info(f"Dist Per RSSI Max: {self.dist_per_rssi.max}")
+            self.info(f"Dist Per RSSI Size: {self.dist_per_rssi.size}")  
+            self.info(f"Dist Per RSSI Ind: {self.dist_per_rssi._ind}")
+            self.info(f"Precision: {self.dist_per_rssi.precision}")
+            self.info(f"Precision Index 1: {self.dist_per_rssi._precision_ind_1}")
+            self.info(f"Precision Index 2: {self.dist_per_rssi._precision_ind_2}")
+            self.info(f"Dist Per RSSI Data: {self.dist_per_rssi.data}")
+            self.info("~~~~~~~~~~~~~~~~~~~~~~~")
+
+
+            # Update last RSSI value
+            self.last_y = self.x
+            self.last_y = self.y
+            self.last_z = self.z
+
+            # Publisher topic
+            self.pubsub.publish(self.prepend(self.get(SyncPublisher.DIST_PER_UNIT_RSSI_PUB_TOPIC)),
+                                Float64(data=dist_per_unit_rssi))
+
     # Create alias function for getting logger
     def log(self):
         return self.get_logger()
@@ -166,15 +392,41 @@ class SyncPublisher(Node):
     def get(self, param) -> Any:
         return self.get_parameter(param).value
 
-    # TODO
     def extract_x_y_values(self, msg) -> None:
         self.x = msg.x
         self.y = msg.y
     
-    # TODO
     def extract_z_value(self, msg) -> None:
         self.z = msg.data
+        self.update_rssi_stats(msg)
 
+    @staticmethod
+    def extract_time(time: Time, digits: int = 3) -> float:
+        return round(time.sec + time.nanosec * 1e-9, digits)
+
+
+    def update_rssi_stats(self, msg):
+
+        current_stamp: float = self.extract_time(self.get_clock().now().to_msg())
+        freq = round(1/(current_stamp - self.prev_stamp), 3)
+        self.freq.append(freq)
+        self.prev_stamp = current_stamp
+
+        self.info(f"Avg: {self.freq.avg}")
+        self.info(f"Std: {self.freq.std}")
+        self.info(f"Min: {self.freq.min}")
+        self.info(f"Max: {self.freq.max}")
+        self.info(f"Size: {self.freq.size}")  
+        self.info(f"Ind: {self.freq._ind}")
+        self.info(f"Precision: {self.freq.precision}")
+        self.info(f"Precision Index 1: {self.freq._precision_ind_1}")
+        self.info(f"Precision Index 2: {self.freq._precision_ind_2}")
+        self.info(f"Data: {self.freq.data}")
+        self.info("~~~~~~~~~~~~~~~~~~~~~~~")
+
+        # Publish RSSI freq topic
+        self.pubsub.publish(self.prepend(self.get(SyncPublisher.UPDATE_FREQ_PUB_TOPIC)),
+                            Float64(data=freq))
 
 
 def main(args=None):
